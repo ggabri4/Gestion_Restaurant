6bcd1f5d762e3688f6addb07914a6fb9e4d19fcc
Steven <carliersteven80240@gmail.com>
2023-03-09 09:51:55 +0100

    resolution merge

--- Gestion_Restaurant/Pages/Factures/Index.cshtml.cs
+++ Gestion_Restaurant/Pages/Factures/Index.cshtml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -34,38 +33,3 @@ namespace Gestion_Restaurant.Pages.Factures
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Gestion_Restaurant.Data;
-using Gestion_Restaurant.Models;
-
-namespace Gestion_Restaurant.Pages.Factures
-{
-    public class IndexModel : PageModel
-    {
-        private readonly Gestion_Restaurant.Data.ApplicationDbContext _context;
-
-        public IndexModel(Gestion_Restaurant.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Facture> Facture { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            if (_context.Facture != null)
-            {
-                Facture = await _context.Facture
-                .Include(f => f.CommandeFacturer).ToListAsync();
-            }
-        }
-    }
-}
->>>>>>> b06c15071445cebbe8ef6d1b7a465636dfe129c6
--- Gestion_Restaurant/Pages/Tables/Details.cshtml.cs
+++ Gestion_Restaurant/Pages/Tables/Details.cshtml.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -44,48 +43,3 @@ namespace Gestion_Restaurant.Pages.Tables
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Gestion_Restaurant.Data;
-using Gestion_Restaurant.Models;
-
-namespace Gestion_Restaurant.Pages.Tables
-{
-    public class DetailsModel : PageModel
-    {
-        private readonly Gestion_Restaurant.Data.ApplicationDbContext _context;
-
-        public DetailsModel(Gestion_Restaurant.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-      public Table Table { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null || _context.Table == null)
-            {
-                return NotFound();
-            }
-
-            var table = await _context.Table.FirstOrDefaultAsync(m => m.Id == id);
-            if (table == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Table = table;
-            }
-            return Page();
-        }
-    }
-}
->>>>>>> b06c15071445cebbe8ef6d1b7a465636dfe129c6
